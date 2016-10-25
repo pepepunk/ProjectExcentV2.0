d8ac4fea7b9048ab16eeafe6557b0dbeed8a9021
pepepunk <jmppk95@hotmail.com>
2016-10-24 19:17:15 -0500

    Cambios

--- app/src/main/java/com/ppk/app/projectexcent/Main2Activity.java
+++ app/src/main/java/com/ppk/app/projectexcent/Main2Activity.java
@@ -2,12 +2,17 @@ package com.ppk.app.projectexcent;
 
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 public class Main2Activity extends AppCompatActivity {
     private Button b1,b2,b3,b4;
@@ -16,6 +21,23 @@ public class Main2Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        TextView texto1=(TextView) findViewById(R.id.id1);
+
+        Calendar calendario = new GregorianCalendar();
+        int agno=calendario.get(Calendar.YEAR);
+        int mes=calendario.get(Calendar.MONTH)+1;
+        int dia= calendario.get(Calendar.DAY_OF_MONTH);
+        int hora= calendario.get(Calendar.HOUR_OF_DAY);
+        int minuto=calendario.get(Calendar.MINUTE);
+        String horaa= Integer.toString(hora);
+
+
+        if (agno==2016 && mes>=10 && dia>27) {
+
+            if (hora >= 0 && minuto >= 0) {
+                texto1.setText("!Gracias por haber asistido al CONAINTE ITSOEH 2016!");
+            }
+        }
 
         b1=(Button) findViewById(R.id.bt1);
         b2=(Button) findViewById(R.id.bt2);
